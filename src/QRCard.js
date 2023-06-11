235d40bd5dc40be9387d3a3f16913f0d54d26f64
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2023-06-11 17:20:44 +0200

    Final commit

---
+++ src/QRCard.js
@@ -0,0 +1,12 @@
+import './QRCard.css';
+export default function QRCard() {
+    return (
+        <div className="qr-card">
+            <img src="/image-qr-code.png" alt="qr code" className="qr-image"/>
+            <div className="qr-card-text">
+                <h2 className="qr-heading">Improve your front-end skills by building projects</h2>
+                <p className="qr-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </div>
+    )
+}
