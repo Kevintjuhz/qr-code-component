fea1229f88227a56a933cddce1565a311d45703b
Kevin Quaedvlieg <k.quaedvlieg@outlook.com>
2023-06-11 21:22:44 +0200

    Change to direct url

--- src/QRCard.js
+++ src/QRCard.js
@@ -1,8 +1,10 @@
 import './QRCard.css';
+import './image-qr-code.png'
+
 export default function QRCard() {
     return (
         <div className="qr-card">
-            <img src="./qr-code-component/image-qr-code.png" alt="qr code" className="qr-image"/>
+            <img src="./image-qr-code.png" alt="qr code" className="qr-image"/>
             <div className="qr-card-text">
                 <h2 className="qr-heading">Improve your front-end skills by building projects</h2>
                 <p className="qr-text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
